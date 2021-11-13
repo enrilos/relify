3a480e9995ec3097642c5d771110dfcf747d0db4
emrrar <avatareigan@gmail.com>
2021-11-13 12:36:11 +0200

    Removed typography.css due to its interference with CSS Modules.

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css';
-import '../../styles/typography.css';
 
 const Header = () => {
     return (
         <header className={styles['container-header']}>
             <section className={styles['container-header-logo']}>
-                <Link to="/" className="container-header-logo-text">relify</Link>
+                <Link to="/" className={styles['container-header-logo-text']}>relify</Link>
             </section>
             <ul>
                 <li><Link to="/stories">Stories</Link></li>
                 <li><Link to="/myStories">My Stories</Link></li>
                 <li><Link to="/myLikes">My Likes</Link></li>
+                <li><Link to="/myFavourites">My Favourites</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
             <ul>
-                <li><Link to="/login">Log in</Link></li>
+                <li><Link to="/login">Login</Link></li>
                 <li><Link to="/register">Register</Link></li>
                 <li><Link to="#">Logout</Link></li>
             </ul>
