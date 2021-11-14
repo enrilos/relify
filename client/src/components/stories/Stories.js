e40fdb0f2e0ffb8cccaf654f5de4ffc2711b9b84
emrrar <avatareigan@gmail.com>
2021-11-14 14:14:38 +0200

    AJAX in conjunction with REST API are functioning as expected.

--- client/src/components/stories/Stories.js
+++ client/src/components/stories/Stories.js
@@ -1,29 +1,35 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import storyService from '../../services/storyService';
+
 import StoryCard from '../storyCard/StoryCard';
 import styles from './Stories.module.css';
 
 const Stories = () => {
-    // const [stories, setStories] = useState([]);
+    const [stories, setStories] = useState([]);
 
-    // useEffect(() => {
-        // get data with custom storyService.js which in turn uses abstract fetch helper (jsonRequest.js)
-    // }, []);
+    useEffect(() => {
+        (async () => {
+            const response = await storyService.getAll();
+            setStories(response);
+        })();
+    }, []);
 
     return (
-        <section className={styles['container-stories']}>
-            <StoryCard
-                key={1} // hardcoded for demo purposes
-                title="First Title"
-                content="Some first desc"
-                author="Brandon"
-            />
-            <StoryCard
-                key={2} // hardcoded for demo purposes
-                title="Second Title http"
-                content="Http vs Https secure tunnel"
-                author="Ron"
-            />
-        </section>
+        <article>
+            <section className={styles['container-intro']}>
+                <h1>Latest stories</h1>
+            </section>
+            <section className={styles['container-stories']}>
+                {stories.map(x =>
+                    <StoryCard
+                        key={x.id}
+                        title={x.title}
+                        content={x.content}
+                        author={x.author}
+                    />
+                )}
+            </section>
+        </article>
     )
 }
 
