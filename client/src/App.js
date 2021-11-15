6ab834501792db913fd2fce21979c59f77bfcceb
emrrar <avatareigan@gmail.com>
2021-11-15 20:35:07 +0200

    Made a sample login component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Stories from './components/stories/Stories';
+import Login from './components/login/Login';
 import Footer from './components/footer/Footer';
 import './App.css';
 
@@ -10,7 +11,8 @@ function App() {
             <Header />
 
             <Routes>
-                <Route exact path="/" element={<Stories/>} />
+                <Route exact path="/" element={<Stories />} />
+                <Route exact path="/login" element={<Login />} />
             </Routes>
 
             <Footer />
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,10 +1,23 @@
-const Login = ({ history }) => {
+import '../../styles/style.css';
+
+const Login = () => {
     const submitHandler = (e) => {
         e.preventDefault();
+
+        const { title, content } = e.target;
+        console.log(title.value);
+        console.log(content.value);
     }
 
     return (
-        <section>
+        <section className="container-auth">
+            <form className="container-auth-form" onSubmit={submitHandler}>
+                <label htmlFor="title">Title</label>
+                <input id="title" type="text" name="title" />
+                <label htmlFor="content">Content</label>
+                <input id="content" type="text" name="content" />
+                <input className="container-auth-form-submit" type="submit" value="Submit" />
+            </form>
         </section>
     );
 }
