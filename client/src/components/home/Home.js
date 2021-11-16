0dc2dd1ba2e79b4cffb12074ad31dd3bb8445651
emrrar <avatareigan@gmail.com>
2021-11-16 16:56:31 +0200

    Updated the Home component.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,7 +2,7 @@ import '../../styles/style.css';
 
 const Home = () => {
     return (
-        <section className="container-intro">
+        <section className="container-home-intro">
             <h1>Escape reality.</h1>
             <h1>Dive into the mysterious world.</h1>
         </section>
