90a31c929c90c4c2ffe21231c50e2345bffc124f
emrrar <avatareigan@gmail.com>
2021-11-20 20:02:13 +0200

    Added a redirect arrow.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import storyService from '../../services/storyService';
 
 import HomeStoryCard from '../homeStoryCard/HomeStoryCard';
@@ -13,9 +14,17 @@ const Home = () => {
 
     return (
         <section className={styles['container-home-intro']}>
-            <h1>Escape reality.</h1>
-            <h1>Dive into the mysterious world.</h1>
-            <p>Latest stories</p>
+            <section className={styles['container-home-intro-title']}>
+                <h1 className={styles['container-home-intro-title-text']}>Escape reality.</h1>
+            </section>
+            <section className={styles['container-home-intro-subtitle']}>
+                <h1 className={styles['container-home-intro-subtitle-text']}>Share your mysterious <span className={styles['highlight-blue']}>stories</span> with other <span className={styles['highlight-green']}>individuals</span> <span className={styles['highlight-purple']}>conveniently</span>.</h1>
+            </section>
+            <section className={styles['container-home-intro-description']}>
+                <p>Why relify?</p>
+                {/* use a consice description of the project Introduction from github */}
+            </section>
+            <p className={styles['latest-stories']}>Latest stories</p>
             <section className={styles['container-stories-home']}>
                 {stories.map(x =>
                     <HomeStoryCard
@@ -25,6 +34,17 @@ const Home = () => {
                         content={x.content}
                     />
                 )}
+                <Link to="/stories" className={styles['container-wrapper']}>
+                    <div className={styles['wrapper']}>
+                        <svg width="18px" height="17px" viewBox="-1 0 18 17">
+                            <g>
+                                <polygon className={styles['arrow']} points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+                                <polygon className={styles['arrow-fixed']} points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+                                <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
+                            </g>
+                        </svg>
+                    </div>
+                </Link>
             </section>
         </section>
     );
