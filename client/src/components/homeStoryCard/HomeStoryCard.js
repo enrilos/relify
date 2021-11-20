24fb0bb163f61e05e286e1c88b3f43178fcd8b50
emrrar <avatareigan@gmail.com>
2021-11-20 20:02:59 +0200

    Major Home changes.

--- client/src/components/homeStoryCard/HomeStoryCard.js
+++ client/src/components/homeStoryCard/HomeStoryCard.js
@@ -9,7 +9,7 @@ const HomeStoryCard = ({
     return (
         <Link className={styles['container-story-wrapper']} to={"/details/" + id}>
             <article className={styles['container-story-home']}>
-                <p className={styles['container-story-home-title']}>{title}</p>
+                <h1 className={styles['container-story-home-title']}>{title}</h1>
                 <p className={styles['container-story-home-content']}>{content}</p>
                 {/* The below link will lead to a details page where the user can like/favourite/comment */}
                 {/* <Link to={"/details/" + id}>Read full</Link> */}
