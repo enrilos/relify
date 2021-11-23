208d24e34e9abafd2625fa5106e3b1907ef9f2c8
emrrar <avatareigan@gmail.com>
2021-11-23 16:23:45 +0200

    Made the Stories listing style a flexbox.

--- client/src/components/storyCard/StoryCard.js
+++ client/src/components/storyCard/StoryCard.js
@@ -7,14 +7,11 @@ const StoryCard = ({
     content,
     type
 }) => {
-    // Reuse code. combine HomeStoryCard with StoryCard.
     return (
         <Link to={"/details/" + id} className={type === 'home' ? styles['container-home-story-wrapper'] : styles['container-stories-story-wrapper']}>
             <article className={styles['container-story-card']}>
                 <h1 className={styles['container-story-card-title']}>{title}</h1>
                 <p className={styles['container-story-card-content']}>{content}</p>
-                {/* The below link will lead to a details page where the user can like/favourite/comment */}
-                {/* <Link to={"/details/" + id}>Read full</Link> */}
             </article>
         </Link>
     );
