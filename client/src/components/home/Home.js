dd8995aee9163478bdf058dc674debf6191455df
emrrar <avatareigan@gmail.com>
2021-11-28 19:41:32 +0200

    Dynamic arrow only shows if there are stories.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -33,27 +33,29 @@ const Home = () => {
                         ?
                         <p className={styles['no-stories']}>No stories yet.</p>
                         :
-                        stories.map(x =>
-                            <StoryCard
-                                key={x._id}
-                                id={x._id}
-                                title={x.title}
-                                content={x.content}
-                                type="home"
-                            />
-                        )
+                        <>
+                            {stories.map(x =>
+                                <StoryCard
+                                    key={x._id}
+                                    id={x._id}
+                                    title={x.title}
+                                    content={x.content}
+                                    type="home"
+                                />
+                            )}
+                            <Link to="/stories" className={styles['container-wrapper']}>
+                                <div className={styles['wrapper']}>
+                                    <svg width="18px" height="17px" viewBox="-1 0 18 17">
+                                        <g>
+                                            <polygon className={styles['arrow']} points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+                                            <polygon className={styles['arrow-fixed']} points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+                                            <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
+                                        </g>
+                                    </svg>
+                                </div>
+                            </Link>
+                        </>
                 }
-                <Link to="/stories" className={styles['container-wrapper']}>
-                    <div className={styles['wrapper']}>
-                        <svg width="18px" height="17px" viewBox="-1 0 18 17">
-                            <g>
-                                <polygon className={styles['arrow']} points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
-                                <polygon className={styles['arrow-fixed']} points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
-                                <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
-                            </g>
-                        </svg>
-                    </div>
-                </Link>
             </section>
         </section>
     );
