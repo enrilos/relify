462b38dbc7bb03665115d20b6c2769c51ca7ac3d
emrrar <avatareigan@gmail.com>
2021-11-28 17:50:36 +0200

    Added dynamic form validation in CreateStory.js

--- client/src/components/createStory/CreateStory.js
+++ client/src/components/createStory/CreateStory.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import storyService from '../../services/storyService';
 import styles from './CreateStory.module.css';
@@ -5,38 +6,59 @@ import styles from './CreateStory.module.css';
 const CreateStory = () => {
     let navigate = useNavigate();
 
+    const [isTitleValid, setIsTitleValid] = useState(true);
+    const [isContentValid, setIsContentValid] = useState(true);
+
+    const onBlueTitle = (e) => {
+        const value = e.target.value;
+
+        if (value.length >= 3 && value.length <= 32) {
+            setIsTitleValid(true);
+        } else {
+            setIsTitleValid(false);
+        }
+    }
+
+    const onBlurContent = (e) => {
+        const value = e.target.value;
+
+        if (value.length >= 3 && value.length <= 2048) {
+            setIsContentValid(true);
+        } else {
+            setIsContentValid(false);
+        }
+    }
+
     const submitHandler = async (e) => {
         e.preventDefault();
 
-        const { title, content } = e.target;
-
-        if (!title.value || !content.value) {
-            window.alert("All fields are required.");
-            return;
+        if (isTitleValid && isContentValid) {
+            
+            const { title, content } = e.target;
+            
+            const story = {
+                title: title.value,
+                content: content.value
+            };
+            
+            await storyService.create(story);
+            navigate("/");
         }
-
-        const story = {
-            title: title.value,
-            content: content.value
-        };
-
-        await storyService.create(story);
-        navigate("/");
     }
     
-    // TODO: Active input box-shadow
-
     return (
         <form className={styles['container-standard-form']} onSubmit={submitHandler}>
             <fieldset>
                 <legend>Create Story</legend>
                 <p>
                     <label htmlFor="title">Title</label>
-                    <input id="title" type="text" name="title" />
+                    <input id="title" type="text" name="title" onBlur={onBlueTitle} />
+                    {!isTitleValid ? <span className={styles['error-message']}>Title must be between 3 and 32 characters.</span> : null}
                 </p>
                 <p>
                     <label htmlFor="content">Content</label>
-                    <textarea className={styles['container-standard-form-content']} id="content" type="text" name="content"></textarea>
+                    <textarea className={styles['container-standard-form-content']} id="content" type="text" name="content" onBlur={onBlurContent}></textarea>
+                    {!isContentValid ? <span className={styles['error-message']}>Content must be between 3 and 2048 characters.</span> : null}
                 </p>
                 <input className={styles['container-standard-form-submit']} type="submit" value="Submit" />
             </fieldset>
