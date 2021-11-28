79354b7651069593ed062af9dc82f6954153c8f3
emrrar <avatareigan@gmail.com>
2021-11-28 17:52:44 +0200

    Added dynamic form validation in EditStory.js

--- client/src/components/editStory/EditStory.js
+++ client/src/components/editStory/EditStory.js
@@ -9,10 +9,32 @@ const EditStory = () => {
     let { storyId } = useParams();
 
     const [story, setStory] = useState({});
+    const [isTitleValid, setIsTitleValid] = useState(true);
+    const [isContentValid, setIsContentValid] = useState(true);
 
     useEffect(() => {
         storyService.get(storyId).then(x => setStory(x));
     }, []);
+
+    const onBlueTitle = (e) => {
+        const value = e.target.value;
+
+        if (value.length >= 3 && value.length <= 32) {
+            setIsTitleValid(true);
+        } else {
+            setIsTitleValid(false);
+        }
+    }
+
+    const onBlurContent = (e) => {
+        const value = e.target.value;
+
+        if (value.length >= 3 && value.length <= 2048) {
+            setIsContentValid(true);
+        } else {
+            setIsContentValid(false);
+        }
+    }
 
     const submitHandler = async (e) => {
         e.preventDefault();
@@ -40,11 +62,13 @@ const EditStory = () => {
                 <legend>Edit Story</legend>
                 <p>
                     <label htmlFor="title">Title</label>
-                    <input id="title" type="text" name="title" defaultValue={story.title} />
+                    <input id="title" type="text" name="title" defaultValue={story.title} onBlur={onBlueTitle} />
+                    {!isTitleValid ? <span className={styles['error-message']}>Title must be between 3 and 32 characters.</span> : null}
                 </p>
                 <p>
                     <label htmlFor="content">Content</label>
-                    <textarea className={styles['container-standard-form-content']} id="content" type="text" name="content" defaultValue={story.content}></textarea>
+                    <textarea className={styles['container-standard-form-content']} id="content" type="text" name="content" defaultValue={story.content} onBlur={onBlurContent}></textarea>
+                    {!isContentValid ? <span className={styles['error-message']}>Content must be between 3 and 2048 characters.</span> : null}
                 </p>
                 <input className={styles['container-standard-form-submit']} type="submit" value="Submit" />
             </fieldset>
