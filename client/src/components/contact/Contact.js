417284510b4a5121cbc01c67ceb08119b5c5eda7
emrrar <avatareigan@gmail.com>
2021-12-01 19:02:55 +0200

    Finished Contact page.

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -3,7 +3,15 @@ import styles from './Contact.module.css';
 const Contact = () => {
     return (
         <section className={styles['container-contact']}>
-            <h1 className={styles['container-contact-title']}>This is contact page.</h1>
+            <article className={styles['container-contact-description']}>
+                <h1 className={styles['container-contact-description-title']}>How can I reach out to you?</h1>
+                <p className={styles['container-contact-description-subtitle']}>You can email us at <a href="mailto:relify@office.com">relify@office.com</a></p>
+                <p className={styles['container-contact-description-subtitle']}>Our office is located in Sofia, Bulgaria - Litex Tower, 1st floor</p>
+                <p className={styles['container-contact-description-subtitle']}>+359 123 456 789</p>
+                <p className={styles['container-contact-description-subtitle']}>PK: 1337</p>
+                <p className={styles['container-contact-description-subtitle']}>Workweek: 9:00 - 19:30</p>
+                <p className={styles['container-contact-description-subtitle']}>Weekend: 10:00 - 17:00</p>
+            </article>
         </section>
     );
 }
