71819f914c99893ea4becb5a8a31f3f17fc12ae3
emrrar <avatareigan@gmail.com>
2021-12-02 16:14:50 +0200

    Added dynamic search bar in MyStories.js

--- client/src/components/myStories/MyStories.js
+++ client/src/components/myStories/MyStories.js
@@ -11,16 +11,40 @@ const MyStories = () => {
         storyService.getUserStories(authApi.getUserId()).then(x => setStories(x)).catch(err => console.error(err));
     }, [])
 
+    function debounce(func, timeout = 250) {
+        let timer;
+        return (e) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => { func.call(null, e); }, timeout);
+        }
+    }
+
+    const onSearchHandler = (e) => {
+        const title = e.target.value.trim();
+
+        if (title) {
+            storyService.getUserStoriesByTitle(title, authApi.getUserId()).then(x => setStories(x)).catch(err => console.error(err));
+        } else {
+            // This, however, makes a redundant request on every manually inserted white space.
+            storyService.getUserStories(authApi.getUserId()).then(x => setStories(x)).catch(err => console.error(err));
+        }
+    }
+
+    const searchChange = debounce((e) => onSearchHandler(e));
+
     // TODO: Infinite scroller?
     return (
         <section>
-            {
-                stories.length === 0
-                    ?
-                    <h1 className={styles['container-stories-title']}>No stories.</h1>
-                    :
-                    <h1 className={styles['container-stories-title']}>My Stories</h1>
-            }
+            <section className={styles['container-header-stories']}>
+                {
+                    stories.length === 0
+                        ?
+                        <h1 className={styles['container-header-stories-title']}>No stories.</h1>
+                        :
+                        <h1 className={styles['container-header-stories-title']}>My Stories</h1>
+                }
+                <input type="text" name="title" placeholder="Search..." onKeyUp={searchChange} />
+            </section>
             <section className={styles['container-stories']}>
                 {stories.map(x =>
                     <StoryCard
